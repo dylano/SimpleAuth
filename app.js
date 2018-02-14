ce01bf50fa2342a686adee6cd08ae3d403fc78f6
Dylan Oliver <doliver@dylans-mbp.lan>
2018-02-14 05:44:18 -0800

    + logout

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/secret', function(req, res) {
+app.get('/secret', isLoggedIn, function(req, res) {
     res.render('secret');
 });
 
@@ -61,6 +61,18 @@ app.post('/login', passport.authenticate('local', {
         successRedirect: '/secret',
         failureRedirect: '/login'}), function(req, res) {
 });
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn (req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(PORT, function(){
     console.log(__dirname + ' listening on port ' + PORT + '...');
